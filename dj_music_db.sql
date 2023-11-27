3e13ca7a25e189fd8cc23fbe10e815885ee054eb
ahadout <mohamedahdoutt@gmail.com>
2023-11-27 14:54:40 +0100

    database updated

--- dj_music_db.sql
+++ dj_music_db.sql
@@ -1,242 +1,121 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Nov 17, 2023 at 04:38 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Creating the albums table
+CREATE TABLE albums (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  release_date DATE NOT NULL,
+  cover_image_s3_url VARCHAR(255)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Creating the album_tracks table without FK constraints
+CREATE TABLE album_tracks (
+  album_id INT,
+  track_id INT,
+  PRIMARY KEY (album_id, track_id)
+);
 
+-- Creating the orders table without FK constraints
+CREATE TABLE orders (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  product_id INT,
+  quantity INT NOT NULL,
+  total_price DECIMAL(10, 2) NOT NULL,
+  order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  tracking_number VARCHAR(50),
+  status ENUM('pending', 'complete', 'shipped', 'delivered'),
+  address VARCHAR(255),
+  payment_method VARCHAR(50),
+  payment_status ENUM('pending', 'paid', 'failed', 'refunded')
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Creating the products table
+CREATE TABLE products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  short_description VARCHAR(255),
+  description TEXT,
+  price DECIMAL(10, 2) NOT NULL,
+  image VARCHAR(255),
+  type ENUM('cloud', 'hard_drive'),
+  quantity_in_stock INT
+);
 
---
--- Database: `dj_music_db`
---
+-- Creating the subscriptions table without FK constraints
+CREATE TABLE subscriptions (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  product_id INT,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
+);
 
--- --------------------------------------------------------
+-- Creating the tracks table
+CREATE TABLE tracks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  category VARCHAR(50),
+  duration TIME NOT NULL,
+  s3_url VARCHAR(255) NOT NULL
+);
 
---
--- Table structure for table `albums`
---
+-- Creating the Downloads table without FK constraints
+CREATE TABLE Downloads (
+  track_id INT,
+  user_id INT,
+  download_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (track_id, user_id)
+);
 
-CREATE TABLE `albums` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `release_date` date NOT NULL,
-  `cover_image_s3_url` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Creating the users table
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password_hash VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  full_name VARCHAR(255),
+  user_role ENUM('new_user', 'admin', 'subscriber'),
+  address VARCHAR(255),
+  phone VARCHAR(20),
+  country VARCHAR(50)
+);
 
--- --------------------------------------------------------
+-- Creating the Reviews table without FK constraints
+CREATE TABLE Reviews (
+  review_id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  user_id INT,
+  review TEXT,
+  stars INT
+);
 
---
--- Table structure for table `album_tracks`
---
+-- Creating the newsletter table
+CREATE TABLE newsletter (
+  email VARCHAR(100) PRIMARY KEY,
+  subscription_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE `album_tracks` (
-  `album_id` int(11) NOT NULL,
-  `track_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Adding FK constraints to album_tracks
+ALTER TABLE album_tracks
+ADD FOREIGN KEY (album_id) REFERENCES albums(id),
+ADD FOREIGN KEY (track_id) REFERENCES tracks(id);
 
--- --------------------------------------------------------
+-- Adding FK constraints to orders
+ALTER TABLE orders
+ADD FOREIGN KEY (user_id) REFERENCES users(id),
+ADD FOREIGN KEY (product_id) REFERENCES products(id);
 
---
--- Table structure for table `orders`
---
+-- Adding FK constraints to subscriptions
+ALTER TABLE subscriptions
+ADD FOREIGN KEY (user_id) REFERENCES users(id),
+ADD FOREIGN KEY (product_id) REFERENCES products(id);
 
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` int(11) DEFAULT 1,
-  `total_price` decimal(10,2) NOT NULL,
-  `order_time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `tracking_number` varchar(50) DEFAULT NULL,
-  `status` enum('pending','complete','shipped','delivered') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Adding FK constraints to Downloads
+ALTER TABLE Downloads
+ADD FOREIGN KEY (track_id) REFERENCES tracks(id),
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` text DEFAULT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `type` enum('cloud','hard_drive') NOT NULL,
-  `subscription` tinyint(1) DEFAULT 0,
-  `renew_price` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `subscriptions`
---
-
-CREATE TABLE `subscriptions` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tracks`
---
-
-CREATE TABLE `tracks` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `duration` time NOT NULL,
-  `s3_url` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `full_name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `albums`
---
-ALTER TABLE `albums`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `album_tracks`
---
-ALTER TABLE `album_tracks`
-  ADD PRIMARY KEY (`album_id`,`track_id`),
-  ADD KEY `track_id` (`track_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `subscriptions`
---
-ALTER TABLE `subscriptions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `tracks`
---
-ALTER TABLE `tracks`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `albums`
---
-ALTER TABLE `albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `subscriptions`
---
-ALTER TABLE `subscriptions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tracks`
---
-ALTER TABLE `tracks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `album_tracks`
---
-ALTER TABLE `album_tracks`
-  ADD CONSTRAINT `album_tracks_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`),
-  ADD CONSTRAINT `album_tracks_ibfk_2` FOREIGN KEY (`track_id`) REFERENCES `tracks` (`id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
-
---
--- Constraints for table `subscriptions`
---
-ALTER TABLE `subscriptions`
-  ADD CONSTRAINT `subscriptions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Adding FK constraints to Reviews
+ALTER TABLE Reviews
+ADD FOREIGN KEY (product_id) REFERENCES products(id),
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
