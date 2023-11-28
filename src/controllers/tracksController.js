fb6f98e2421928b5d5e426a29125839c9323494b
ahadout <mohamedahdoutt@gmail.com>
2023-11-28 13:12:02 +0100

    get tracks from its category

--- src/controllers/tracksController.js
+++ src/controllers/tracksController.js
@@ -10,3 +10,18 @@ exports.getAllTracks = async (req, res) => {
     res.status(500).send('Error fetching tracks');
   }
 };
+
+exports.getCategoryTracks = async (req, res) => {
+    const category = req.params.category;
+  
+    try {
+      const [tracks] = await db.query('SELECT * FROM tracks WHERE category = ?', [category]);
+      if (tracks.length === 0) {
+        return res.status(404).send('No tracks found in this category');
+      }
+      res.json(tracks);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching tracks from the category');
+    }
+};
--- src/routes/tracksRoutes.js
+++ src/routes/tracksRoutes.js
@@ -5,5 +5,6 @@ const tracksController = require('../controllers/tracksController');
 
 // Route to get all tracks
 router.get('/', tracksController.getAllTracks);
+router.get('/category/:category', tracksController.getCategoryTracks);
 
 module.exports = router;
