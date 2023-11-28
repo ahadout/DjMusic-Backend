5ef40f18918bf7293ce3d8e9bd537358eabaed7e
ahadout <mohamedahdoutt@gmail.com>
2023-11-28 11:48:48 +0100

    featured product added

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -14,11 +14,24 @@ exports.getProduct = async (req, res) => {
   try {
     const [product] = await db.query('SELECT * FROM products WHERE id = ?', [req.params.id]);
     if (product.length === 0) {
-      res.status(404).send('Product not found');
+      res.status(404).send('Product ID not found');
     } else {
       res.json(product[0]);
     }
   } catch (error) {
     res.status(500).send('Error fetching product');
+  }
+};
+
+exports.getFeaturedProduct = async (req, res) => {
+  try {
+    const [featuredProduct] = await db.query('SELECT * FROM products WHERE Featured = 1');
+    if (featuredProduct.length === 0) {
+      res.status(404).send('Featured product not found');
+    } else {
+      res.json(featuredProduct[0]);
+    }
+  } catch (error) {
+    res.status(500).send('Error fetching featured product');
   }
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.getProducts);
+router.get('/featured', productController.getFeaturedProduct);
 router.get('/:id', productController.getProduct);
 
 module.exports = router;
